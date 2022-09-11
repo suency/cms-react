fa691e2d356b178b22b07762be11bc8c07e69b61
更更爱卖萌 <ioioiolili@126.com>
2022-09-11 21:02:41 +0800

    init time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Layout from './pages/Layout'
 
 function App () {
   return (
-    <div>
-      1
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Layout />}></Route>
+          <Route path="/Login" element={<Login />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
